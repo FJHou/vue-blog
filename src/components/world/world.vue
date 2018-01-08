9eafafe087a29477236bcdf1e12c929b8268b74e
houfujian <ApeLiftingIron@163.com>
2018-01-08 19:53:04 +0800

    a

--- src/components/world/world.vue
+++ src/components/world/world.vue
@@ -2,7 +2,7 @@
   <div class="world" v-show="showFlag">
     <back></back>
     <div id="earth" class="earth" ref="earth"></div>
-    <!-- <canvas id="galaxy" class="galaxy"></canvas> -->
+    <canvas id="galaxy" class="galaxy"></canvas>
   </div>
 </template>
 
@@ -28,35 +28,9 @@ export default {
   mounted () {
     let earth = new Earth('earth')
     this.$options = earth
-    // this._creatGalaxy()
-    // this._starAnimate()
-    // var cvs = document.getElementsByName('canvas')
-    // console.log(earth)
   },
   methods: {
-    // _creatGalaxy () {
-    //   this.canvas = document.getElementById('galaxy')
 
-    //   for (let i = 0; i < maxStars; i++) {
-    //     stars[i] = new Star(this.canvas)
-    //   }
-    // },
-    // _starAnimate () {
-    //   let ctx = this.canvas.getContext('2d')
-    //   let w = 320
-    //   let h = 474
-    //   // 关于globalCompositeOperation http://www.w3school.com.cn/tags/canvas_globalcompositeoperation.asp
-    //   ctx.globalCompositeOperation = 'source-over'
-    //   ctx.globalAlpha = 0.5
-    //   ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)'
-    //   ctx.fillRect(0, 0, w, h)
-
-    //   ctx.globalCompositeOperation = 'lighter'
-    //   for (var i = 1, l = stars.length; i < l; i++) {
-    //     stars[i].draw()
-    //   }
-    //   // window.requestAnimationFrame(this._starAnimate())
-    // }
   },
   components: {
     back
@@ -75,8 +49,9 @@ export default {
     .earth
       width: 100%
       height 100%
+      background-color transparent
   .galaxy
-    z-index 1
+    z-index -1
     position absolute
     top 0
     left 0
